a458dcc0be9b418ee9a5f691923798aad0be5f0d
Vladimir Gordeev <WorkStationGuard@gmail.com>
2024-07-09 17:26:58 +0300

    test: add fixtures

---
+++ src/DataFixtures/AppFixtures.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Blog;
+use App\Entity\User;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+
+class AppFixtures extends Fixture
+{
+    public function __construct(UserPasswordHasherInterface $hasher)
+    {
+        $this->hasher = $hasher;
+    }
+
+    public function load(ObjectManager $manager): void
+    {
+        $user = new User();
+        $user->setEmail('admin@beneficium.ru');
+        $user->setRoles(['ROLE_ADMIN']);
+        $password = $this->hasher->hashPassword($user, 'admin@beneficium.ru');
+        $user->setPassword($password);
+        $manager->persist($user);
+
+        for ($userIndex = 1; $userIndex <= 10; $userIndex++) {
+            $user = new User();
+            $user->setEmail('user'. $userIndex . '@yandex.ru');
+            $password = $this->hasher->hashPassword($user, 'pass_1234');
+            $user->setPassword($password);
+            $manager->persist($user);
+
+            for ($blogIndex = 1; $blogIndex <= 5; $blogIndex++) {
+                $blog = (new Blog($user))
+                    ->setTitle('Blog title' . $userIndex . " " . $blogIndex)
+                    ->setDescription('Blog description' . $userIndex . " " . $blogIndex)
+                    ->setText('Blog text ' . $userIndex . " " . $blogIndex)
+                    ->setUser($user);
+                $manager->persist($blog);
+            }
+        }
+        $manager->flush();
+    }
+}
