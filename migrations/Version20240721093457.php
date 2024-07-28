e12a13d7a849a5fbd329da224598766994f864d8
Vladimir Gordeev <WorkStationGuard@gmail.com>
2024-07-28 17:54:23 +0300

    fix(status): fix migration

--- migrations/Version20240721093457.php
+++ migrations/Version20240721093457.php
@@ -20,14 +20,12 @@ final class Version20240721093457 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE blog ADD status VARCHAR(255) NOT NULL');
-        $this->addSql('ALTER TABLE tags_to_blog RENAME INDEX fk_147ab9dbad26311 TO IDX_147AB9DBAD26311');
+        $this->addSql('ALTER TABLE blog ADD status VARCHAR(255) DEFAULT NULL');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE tags_to_blog RENAME INDEX idx_147ab9dbad26311 TO FK_147AB9DBAD26311');
         $this->addSql('ALTER TABLE blog DROP status');
     }
 }
